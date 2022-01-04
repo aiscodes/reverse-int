2ce853c5ee0ad1c23c434e14770ccae51c263cd7
aiscodes <aigerim6769@gmail.com>
2022-01-04 18:55:52 +0600

    test is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  n = n + ''
+  let reversed = n.split('').reverse().join('')
+  let result = parseInt(reversed, 10)
+  console.log('reversed number: ', result)
+  return result
 }
